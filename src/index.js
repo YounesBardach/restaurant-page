3f0bde9ee6ffb0b634e64a2d91866340403deb31
bardachyounes <bardach.younes@gmail.com>
2022-07-18 06:03:11 +0100

    Build all html pages templates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import './css/normalize.css';
-import './css/home.css';
+import './css/home.css';//to ba adapted to home.js later
+import './img/lenga.svg';
+import './img/mix.svg';
+import './img/original.svg';
+import './img/pepperoni.svg';
+import './img/cheese.svg';
+import './img/sfincione-palermitano.svg';
+import './img/map.svg';
+
+let pageLoad = () => {
+
+    let content = document.getElementById("content")
+    let header = document.createElement("header")
+    let main = document.createElement("main")
+    let div = document.createElement("div")
+    let h1 = document.createElement("h1")
+    let p = document.createElement("p")
+    let button = document.createElement("button")
+    let nav = document.createElement("nav")
+    let ul = document.createElement("ul")
+    let li = document.createElement("li")
+    let a = document.createElement("a")
+    let svg = document.createElement("svg")
+    //svg path
+
+
+
+
+
+
+
+
+    // content.appendChild
+
+}
---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+import flour from '../img/flour.jpg'
+import logoBg from '../img/logo-bg.jpg'
+import pizLogo from '../img/pizlogo.png'
+import pizStar from '../img/pizza-star.png'
+import wood from '../img/wood-bg.jpg'
+import homeStyle from '../css/home.css'
+
